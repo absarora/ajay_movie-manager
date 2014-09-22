15f0a5d4522e04c3dd1efaae25e22c5c3768067d
absarora <absingharora@gmail.com>
2014-09-22 15:47:43 -0700

    Create a New Collection and update/delete it

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -8,8 +8,38 @@ class CollectionsController < ApplicationController
   end
 
   def new
+    @collection = Collection.new
+  end
+
+  def create
+    @collection = Collection.new(collection_params)
+    if @collection.save
+      flash[:notice] = "Collection was saved."
+      redirect_to @collection
+    else
+      flash[:error] = "There was an error saving the Collection. Please try again."
+      render :new
+    end
   end
 
   def edit
+    @collection = Collection.find(params[:id])
+  end
+
+  def update
+    @collection = Collection.find(params[:id])
+    if @collection.update_attributes(collection_params)
+      flash[:notice] = "Collection was updated."
+      redirect_to @collection
+    else
+      flash[:error] = "There was an error saving the collection. Please try again."
+      render :edit
+    end
+  end
+
+  private
+
+  def collection_params
+    params.require(:collection).permit(:title)
   end
 end
